3b34848d892de45121670e5b2f79af1de9b3dd10
sdkfh <rahraj772@gmail.com>
2024-03-16 16:17:17 +0530

    second test commit

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const canvas=document.getElementById("myCanvas");
 canvas.width=200;
-
+//canvas.height=window.innerHeight;
+// isse dikkat hoga 
 const ctx = canvas.getContext("2d");
 const car=new Car(100,100,30,50);
 
 animate();
-
+// test1
 function animate(){
-    car.update();
-    
-    canvas.height=window.innerHeight;
-    car.draw(ctx);
-    requestAnimationFrame(animate);
+  car.update();
+canvas.height=window.innerHeight;
+  car.draw(ctx);
+  requestAnimationFrame(animate);
 }
